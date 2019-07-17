6fdda6a9cdb7ceb0449bdd97f3565dab8bb63c42
Grace Colette <ilovegrace09@gmail.com>
2019-07-17 18:45:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(collection)
 i = 0
+arr = {}
 while collection.length > i 
-  puts yield(collection[i]).split(" ").first
+  puts arr << yield(collection[i]).split(" ").first 
   i += 1
   end
 end
