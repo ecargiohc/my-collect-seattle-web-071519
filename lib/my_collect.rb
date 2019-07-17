cc94224e260825ce36c1c41124a5d2f96ae7ae3e
Grace Colette <ilovegrace09@gmail.com>
2019-07-17 18:41:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
 i = 0
 while collection.length > i 
   # do |name|
-    puts yield(collection[i]).split(" ").first
+    puts yield(collection[i]).split(" ")
     i += 1
   end
 end
